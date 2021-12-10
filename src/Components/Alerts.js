8f890a847a4b39cbe5b8750855017ee850d83f56
DebosmitaMukherjee <93934571+DebosmitaMukherjee@users.noreply.github.com>
2021-12-11 00:27:29 +0530

    Add files via upload

---
+++ src/Components/Alerts.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Alerts(props) {
+  return (
+    props && <div>
+      <div calssName="alert alert-{}" style={{backgroundColor:'grey'}}role="alert">
+        
+      </div>
+    </div>
+  );
+}
+export default Alerts
